5c8b125fd48fa039f1fa72901e4681f9468a1956
MJerich <MatthewJerich@gmail.com>
2022-02-25 17:09:52 -0700

    first question displayed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 let nameFormEl = document.querySelector("#name-form");
-let saveData = {savedName: "", highScore: ""}
+let saveData = {savedName: "", highScore: ""};
 const mainContentEl = document.querySelector("#questions");
+let correctAnswerEl = document.getElementsByClassName("correct");
 
 function startQuiz(event){
     event.preventDefault();
@@ -9,11 +10,15 @@ function startQuiz(event){
     let userNameEl = document.querySelector("input[name='name']").value;
     saveData.savedName = userNameEl
 
-    // remove the maincontent to make room for first question
-    removeContent(mainContentEl);
-
-    // add first question
-    firstQuestion();
+    if (userNameEl === "") {
+        alert("You need to enter a name");
+        return
+    } else {
+        // remove the maincontent to make room for first question
+        removeContent(mainContentEl);
+        // add first question
+        firstQuestion();
+    }
 };
 
 function firstQuestion() {
@@ -23,15 +28,31 @@ function firstQuestion() {
     const oneAnswerTwo = "<js>"
     const oneAnswerThree = "<javascript>"
     const oneAnswerFour = "<scripting>"
+    let answerOneEl = null;
+    let answerTwoEl = null;
+    let answerThreeEl = null;
+    let answerFourEl = null;
+
 
     // add the content the the html
     // create element for the question
     let questionOneEl = document.createElement("h2");
-    questionOneEl.innerHTML = questionOne;
+    questionOneEl.textContent = questionOne;
+    mainContentEl.appendChild(questionOneEl);
 
     // create the buttons
 
-    mainContentEl.appendChild(questionOneEl);
+    mainContentEl.appendChild(createButton(answerOneEl, oneAnswerOne, "correct"));
+    mainContentEl.appendChild(createButton(answerTwoEl, oneAnswerTwo, "wrong"));
+    mainContentEl.appendChild(createButton(answerThreeEl, oneAnswerThree, "wrong"));
+    mainContentEl.appendChild(createButton(answerFourEl, oneAnswerFour, "wrong"));
+}
+
+function createButton(buttonName, buttonStr, answerState) {
+    buttonName = document.createElement("button");
+    buttonName.textContent = buttonStr;
+    buttonName.setAttribute("class", answerState + ", btn");
+    return buttonName;
 }
 
 function removeContent(parentEl) {
@@ -39,4 +60,12 @@ function removeContent(parentEl) {
 
 };
 
+function answerChecker(event) {
+    let element = event.target;
+    if (element.classList.contains("correct")) {
+        console.log("that is the correct answer!!!")
+    }
+}
+
 nameFormEl.addEventListener("submit", startQuiz);
+document.addEventListener("click", answerChecker);
